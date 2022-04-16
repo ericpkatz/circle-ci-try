53c2f9e695feb9044fd70f6c03d9148dbffeab24
ericpkatz <ericpkatz@gmail.com>
2022-04-16 07:36:34 -0700

    database

--- test/spec.js
+++ test/spec.js
@@ -1,4 +1,7 @@
+const Sequelize = require('sequelize');
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db');
 describe('test', ()=> {
+  beforeEach(()=> conn.sync({ force: true }));
   it('works', ()=> {
 
   });
